8aea6b0dad79ffb0603373284e5c6df49e3ebbe4
Victor Poletaev <vpoletaev11@gmail.com>
2022-04-01 15:50:01 +0300

    Rework db connection check.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"blog/handlers"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -12,17 +13,24 @@ import (
 	_ "github.com/jackc/pgx/stdlib"
 )
 
+const (
+	dbConnCheckTimeout = 1 * time.Second
+	dbConnChecksCount  = 20
+)
+
 func main() {
 	db, err := sqlx.Open("pgx", "postgres://postgres:password@172.17.0.1:8081/blog")
 	if err != nil {
-		panic(err)
+		log.Fatalf("error while open connection to database: %s", err.Error())
 	}
 	defer db.Close()
 
-	time.Sleep(30 * time.Second)
-	err = db.Ping()
-	if err != nil {
-		panic(err)
+	for i := 1; db.Ping() != nil; i++ {
+		log.Printf("database acccesabilty check failure")
+		time.Sleep(dbConnCheckTimeout)
+		if i == dbConnChecksCount {
+			log.Fatal("database connection check timeout")
+		}
 	}
 
 	router := mux.NewRouter()
